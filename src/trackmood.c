acf90ae0a27968838e3f713bfa7b150da5cd8a5c
Samuel Rinnetmäki <samuel@iki.fi>
2015-05-03 11:12:23 +0300

    initial commit, basic ui in place

---
+++ src/trackmood.c
@@ -0,0 +1,131 @@
+#include <pebble.h>
+
+static Window *window;
+static TextLayer *greet_layer;
+static TextLayer *mood_layer;
+
+enum Mood {
+  TERRIBLE = 0,
+  NOT_GREAT = 1,
+  OK = 2,
+  GREAT = 3,
+  AWESOME = 4
+};
+
+const char *Times[] = {"morning", "afternoon", "day", "evening, night"};
+const char *Moods[] = {"Terrible", "Not great", "OK", "Great", "Awesome"};
+
+int current_mood = 3;
+int current_time = 2;
+
+static void greet_me() {
+  static char greet_text[40];
+  time_t now = time(NULL);
+  struct tm *tms = localtime(&now);
+  if (tms->tm_hour < 5) {
+    current_time = 4;
+  }
+  else if (tms->tm_hour < 12) {
+    current_time = 0;
+  }
+  else if (tms->tm_hour < 15) {
+    current_time = 1;
+  }
+  else if (tms->tm_hour < 19) {
+    current_time = 2;
+  }
+  else {
+    current_time = 4;
+  }
+  snprintf(greet_text, sizeof(greet_text), "Good %s!\nHow are you feeling?", Times[current_time]);
+  text_layer_set_text(greet_layer, greet_text);
+}
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(mood_layer, Moods[current_mood]);
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  current_mood++;
+  if (current_mood > AWESOME) {
+    current_mood = AWESOME;
+  }
+  text_layer_set_text(mood_layer, Moods[current_mood]);
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  current_mood--;
+  if (current_mood < TERRIBLE) {
+    current_mood = TERRIBLE;
+  }
+  text_layer_set_text(mood_layer, Moods[current_mood]);
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  GRect greet_layer_size = GRect(0, 0, bounds.size.w, 35);
+  greet_layer = text_layer_create(greet_layer_size);
+  text_layer_set_font(greet_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_text_alignment(greet_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(greet_layer));
+  greet_me();
+
+  GRect mood_layer_size = GRect(0, bounds.size.h-30, bounds.size.w, bounds.size.h);
+  mood_layer = text_layer_create(mood_layer_size);
+  text_layer_set_text(mood_layer, Moods[current_mood]);
+  text_layer_set_font(mood_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(mood_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(mood_layer));
+    
+#ifdef PBL_COLOR
+  window_set_background_color(window, GColorOxfordBlue);
+  text_layer_set_text_color(greet_layer, GColorCeleste);
+  text_layer_set_background_color(greet_layer, GColorClear);
+  text_layer_set_text_color(mood_layer, GColorChromeYellow);
+  text_layer_set_background_color(mood_layer, GColorClear);
+#else
+  window_set_background_color(window, GColorBlack);
+  text_layer_set_text_color(greet_layer, GColorWhite);
+  text_layer_set_background_color(greet_layer, GColorClear);
+  text_layer_set_text_color(mood_layer, GColorWhite);
+  text_layer_set_background_color(mood_layer, GColorClear);
+#endif
+
+}
+
+static void window_unload(Window *window) {
+  text_layer_destroy(mood_layer);
+  text_layer_destroy(greet_layer);
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_click_config_provider(window, click_config_provider);
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(window, animated);
+}
+
+static void deinit(void) {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
